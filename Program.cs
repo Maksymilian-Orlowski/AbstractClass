6d9273050301af80fe71faa2f84d7553883e4d2a
Maksymilian-Orlowski <orlowski.maksymilian@op.pl>
2024-09-08 14:25:01 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+        
+        //Coffee coffee = new Coffee();
+        Late late = new Late();
+        Expresso expresso = new Expresso();
+        
+        
+        Console.ReadKey();
+
+        
+    }
+
+
+
+    abstract class Coffee
+    {
+        public int temperature = 0;
+        public int intensive = 0;
+
+    }
+
+    class Late : Coffee
+    {
+        public int temperature = 70;
+        public int intensive = 2;
+    }
+
+    class Expresso : Coffee
+    {
+        public int temperature = 70;
+        public int intensive = 5;
+    }
+
+}
